0d59ec7600dfbe813d6b7091c29f27fbf6e157b4
nycsaif-admin <nycsaif99@gmail.com>
2022-08-23 18:36:34 -0500

    Revert "We use TestNG use for cross browser testing"
    This reverts commit ab12ed19090f3ab523e373cb24224103d882a941.

--- src/modularDrivenFrameWork00/Locators.java
+++
@@ -1,7 +0,0 @@
-package modularDrivenFrameWork00;
-
-public class Locators {
-	public String UserNameLoc= "//input[@id='email']";
-	public String PasswordLoc="//input[@id='pass']";
-	public String LoginButtonLoc="//button[@id='u_0_5_81']"; 
-}
